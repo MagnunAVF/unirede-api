9351ddfe5caf55d3d9f4e38e523f643406310415
Magnun A V F <magnunavf@gmail.com>
2019-04-07 20:07:28 -0300

    test: Create email tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "When created" do
+  context "When creating" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:access_level) }
+
+    it "should validate the correct format of email attribute" do
+      valid_user = build(:user)
+      expect(valid_user.valid?).to eq(true)
+
+      invalid_user_1 = build(:user, email: Faker::Movies::Hobbit.quote)
+      expect(invalid_user_1.valid?).to eq(false)
+      invalid_user_2 = build(:user, email: "foo.gmail.com")
+      expect(invalid_user_2.valid?).to eq(false)
+      invalid_user_3 = build(:user, email: "foo@gmail")
+      expect(invalid_user_3.valid?).to eq(false)
+      invalid_user_4 = build(:user, email: "@gmail.com")
+      expect(invalid_user_4.valid?).to eq(false)
+      invalid_user_5 = build(:user, email: "foo@gmail,com")
+      expect(invalid_user_5.valid?).to eq(false)
+      invalid_user_6 = build(:user, email: "foo@gmail.com!")
+      expect(invalid_user_6.valid?).to eq(false)
+    end
   end
 end
