b6a374435fd6820802538217c35750c0b06f4dd4
Magnun A V F <magnunavf@gmail.com>
2019-04-08 20:48:16 -0300

    feat: Create get user route
    - Route: GET '/users/:id'
    - Also add accepted parameters in user controller;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
     @users = User.all
     json_response(@users)
   end
+
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    json_response(@user)
+  end
+
+  private
+  def user_params
+    params.permit(:name, :email, :password, :access_level)
+  end
 end
