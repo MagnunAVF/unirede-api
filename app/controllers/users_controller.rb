08a6b7f763b6d5021a83d863744c3e9e630978b7
Magnun A V F <magnunavf@gmail.com>
2019-04-09 01:12:54 -0300

    feat: Create update user route
    - Route: PUT 'users/:id'.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :destroy]
+  before_action :set_user, only: [:show, :destroy, :update]
 
   # GET /users
   def index
     @users = User.all
+
     json_response(@users)
   end
 
@@ -22,7 +23,15 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.create!(user_params)
+
     json_response(@user, :created)
+  end
+
+  # PUT /users/:id
+  def update
+    @user.update!(user_params)
+
+    json_response(@user, :accepted)
   end
 
   private
